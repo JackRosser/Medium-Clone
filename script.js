5af48424004ceb48384ec7c2a190f5db6f5a7e4b
Giacomo Rossettini <rossegiac@gmail.com>
2024-09-13 12:06:55 +0200

    css

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let trendingCard = [
     name: "Tom Cooper",
     title: "Ukraine War, 23 October 2022",
     published: "Oct 23",
-    read: " . 5min read"
+    read: " · 5min read"
   }
 ];
 
 for (let i = 0; i < trendingCard.length; i++) {
-  trendingCard[i].num = i + 1;
+  trendingCard[i].num = "0" + (i + 1);
 }
 
 console.log(trendingCard[0].num);

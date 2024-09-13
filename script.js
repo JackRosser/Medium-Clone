c199fb44dfc001ff0715ba1d4e30896efdea2e31
Giacomo Rossettini <rossegiac@gmail.com>
2024-09-13 11:06:01 +0200

    sistemat svg box

---
+++ script.js
@@ -0,0 +1,6 @@
+let elemento = document.querySelector("nav");
+let larghezzaTotale = elemento.offsetWidth;
+let altezzaTotale = elemento.offsetHeight;
+
+console.log("Larghezza totale: ", larghezzaTotale);
+console.log("Altezza totale: ", altezzaTotale);
